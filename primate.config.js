5df26df82e0a7e1cb3650636a9e7d5f36223e816
Anthony Ettinger <anthony@chovy.com>
2023-07-18 15:31:35 -0700

    fix env

--- primate.config.js
+++ primate.config.js
@@ -3,14 +3,26 @@ import surrealdb from "@primate/surrealdb";
 import svelte from "@primate/svelte";
 import session from "@primate/session";
 import {Logger} from "primate";
+import env from "runtime-compat/env";
+
+const { PORT: port, DB_USER: user, DB_PASS: pass, DB_HOST: host, DB_NS: ns, DB_PORT: db_port } = env;
 
 export default {
+  http: {
+    /*
+      ssl: {
+        key: "./ssl/default.key",
+        cert: "./ssl/default.crt",
+      },
+    */
+    port,
+  },
   logger: {
     level: Logger.Info,
   },
   modules: [
     session(),
-    store(surrealdb()),
+    store(surrealdb({ host, port: db_port, ns, user, pass })),
     svelte(),
   ]
 }
