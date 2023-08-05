f5717482c0eb93f5863f46d65465a532a409b217
Anthony Ettinger <anthony@chovy.com>
2023-08-04 23:00:52 -0700

    register page

--- routes/register.js
+++ routes/register.js
@@ -2,6 +2,46 @@ import {view} from "primate";
 
 const form = (params = {}) => view("register/Form.svelte", {...params});
 
+/*
+
+	async create(ctx) {
+		const session = await ctx.session();
+		const user = (await ctx.params()).toObject();
+		const flash = await ctx.flash();
+
+		console.log('create user: ', user);
+
+		if (
+			!user.username ||
+			!user.email ||
+			!user.password ||
+			!user.password2 ||
+			user.password !== user.password2
+		) {
+			// todo figure out how to throw an error here
+			flash.message = 'Unable to signup.';
+			flash.messageType = 'error';
+			return await ctx.redirectTo('account#register');
+		}
+		//todo try/catch
+		const token = await ctx.models.account.create(user);
+
+		session.token = token;
+		session.user = await ctx.models.account.me();
+		ctx.stash.token = token;
+		flash.message = 'Thanks for signing up.';
+
+		await this.generateEmailVerifyCode(ctx);
+
+		if (user.phone) {
+			await this.generatePhoneVerifyCode(ctx);
+		}
+
+		await ctx.redirectTo('account#me');
+	}
+
+  */
+ 
 export default {
   get(request) {
     const {session, query} = request;
