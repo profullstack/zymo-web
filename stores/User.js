0b5e9cb73a9ea6fb87aedd20bd741e0ad06574aa
terrablue <102580937+terrablue@users.noreply.github.com>
2023-08-13 01:16:14 +0300

    stores/User: add custom action create

--- stores/User.js
+++ stores/User.js
@@ -1,4 +1,35 @@
-import {primary} from "@primate/types"
+import env from "@runtime-compat/env";
+import {primary} from "@primate/types";
+
+export const actions = (db, store) => {
+  return {
+    async create(user) {
+      let {email, username, password, password2} = user;
+      const {DB_USER, DB_PASS, DB_NS, DB_DB} = env;
+
+      if (password !== password2) {
+        throw new Error("Passwords do not match");
+      }
+
+      username = username.replace(/[^a-zA-Z0-9]+/gu, "");
+      console.log(user);
+
+      const token = await db.signup({
+        NS: DB_NS,
+        DB: DB_DB,
+        SC: "allusers",
+        email,
+        username,
+        password,
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString(),
+      });
+
+      console.log("token: ", token);
+      return token;
+    },
+  };
+};
 
 export default {
   id: primary,
