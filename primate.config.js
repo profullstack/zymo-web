67c2d2145e9cb2e1a57b2048712f7bbcb27073d9
Anthony Ettinger <anthony@chovy.com>
2023-08-14 20:21:21 -0700

    init

---
+++ primate.config.js
@@ -0,0 +1,27 @@
+import svelte from "@primate/svelte";
+import store from "@primate/store";
+import {surrealdb} from "@primate/store";
+import types from "@primate/types";
+import session from "@primate/session";
+import ws from "@primate/ws";
+import esbuild from "@primate/esbuild";
+
+export default {
+  modules: [
+    svelte(),
+    liveview(),
+    store({
+      strict: true,
+      driver: surrealdb({
+        host: "localhost",
+        port: 5432,
+        ns: "pfdao",
+        db: "pfdao",
+      }),
+    }),
+    types(),
+    session(),
+    ws(),
+    esbuild(),
+  ],
+};
