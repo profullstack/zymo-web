79a89e2f2e5d4517e0a43b86a360f1768242252b
terrablue <102580937+terrablue@users.noreply.github.com>
2023-08-18 14:05:31 +0300

    update register.Form

--- components/register/Form.svelte
+++ components/register/Form.svelte
@@ -9,10 +9,14 @@
     required /></div>
   <div>{data?.errors?.email ?? ""}</div>
   <div><input name="username" placeholder="Enter username" required /></div>
+  <div>{data?.errors?.username ?? ""}</div>
   <div><input name="phone" type="tel" placeholder="Enter phone number" /></div>
+  <div>{data?.errors?.phone ?? ""}</div>
   <div><input name="password" type="password" placeholder="Enter password"
     required /></div>
+  <div>{data?.errors?.password ?? ""}</div>
   <div><input name="password2" type="password" placeholder="Re-enter password"
     required /></div>
+  <div>{data?.errors?.password2 ?? ""}</div>
   <div><button type="submit">Register</button></div>
 </form>
