fd1b8d8c543725504949c2ddcabdcdd56a4aa923
Anthony Ettinger <anthony@chovy.com>
2023-08-18 05:05:58 -0700

    logout button

--- components/NavBar.svelte
+++ components/NavBar.svelte
@@ -1,4 +1,17 @@
+<script>
+    export let data;
+
+    async function logout(e) {
+        e.preventDefault();
+
+        console.log('logout');
+    }
+</script>
 <nav>
-    <a href="/login">Login</a>
-    <a href="/register">Signup</a>
+    {#if data.token}
+        <a href="#" on:click.preventDefault={logout}>Logout</a>
+    {:else}
+        <a href="/login">Login</a>
+        <a href="/register">Signup</a>
+    {/if}
 </nav>
