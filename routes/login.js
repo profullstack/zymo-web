5b1496ec1281bb8dfe419bd971760c1a13a138b0
Anthony Ettinger <anthony@chovy.com>
2023-08-20 06:59:06 -0700

    remove logs from llgin.js

--- routes/login.js
+++ routes/login.js
@@ -8,13 +8,9 @@ export default {
 		const next = query.get('next') || '/dashboard';
 
 		if (session.exists) {
-			// already logged in, redirect to dashboard
-			console.log('next1:', next);
-
 			return redirect(next);
 		}
 
-		// show form
 		return form({ next });
 	},
 	async post(request) {
@@ -24,12 +20,13 @@ export default {
 			login: { Form },
 			User
 		} = store;
-    
+
 		try {
 			const user = request.body.get();
 
 			await Form.validate(user);
-			let token;
+
+      let token;
 			let me;
 
 			try {
@@ -40,7 +37,6 @@ export default {
 			}
 
 			await session.create({ token, user: me, loggedIn: Boolean(token) });
-			console.log('next2:', next);
 
 			return redirect(next);
 		} catch ({ errors }) {
