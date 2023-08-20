29048a2a99a975daa5ef9990312580112a19e218
Anthony Ettinger <anthony@chovy.com>
2023-08-20 13:48:57 -0700

    fix link test

--- routes/a/{alias}.js
+++ routes/a/{alias}.js
@@ -3,13 +3,16 @@ import { redirect } from 'primate';
 export default {
 	async get(request) {
 		const { session, path, store } = request;
-    const { link: { Link} } = store;
-    const alias = path.get('alias');
-    const link = await Link.getByAlias(alias);
+		const {
+			link: { Link }
+		} = store;
+		const alias = path.get('alias');
+		const link = await Link.getByAlias(alias);
+		
 		console.log('alias link2:', link);
 
-    const url = link.url.startsWith("http://") ? link.url : `http://${link.url}`;
+		const url = link.url.startsWith('http') ? link.url : `http://${link.url}`;
 
 		return redirect(url);
-	},
+	}
 };
