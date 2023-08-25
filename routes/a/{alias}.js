4579853f6f8bb6935c0520324df4442be70e5fc8
Anthony Ettinger <anthony@chovy.com>
2023-08-25 01:09:40 -0700

    alias visits

--- routes/a/{alias}.js
+++ routes/a/{alias}.js
@@ -2,15 +2,16 @@ import { redirect } from 'primate';
 
 export default {
 	async get(request) {
-		const { session, path, store } = request;
+		const { session, path, store, headers } = request;
 		const {
 			link: { Link }
 		} = store;
 		const alias = path.get('alias');
 		const link = await Link.getByAlias(alias);
-		
+
 		console.log('alias link2:', link);
 
+		await Link.visit(link, headers);
 		const url = link.url.startsWith('http') ? link.url : `http://${link.url}`;
 
 		return redirect(url);
