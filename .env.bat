e1771b3661e0190405b75ebbba66ce0060192547
Anthony Ettinger <anthony@chovy.com>
2023-09-03 07:57:30 -0700

    windows

---
+++ .env.bat
@@ -0,0 +1,8 @@
+set HOST_USER=ubuntu
+set HOST_DOMAIN=profullstack
+set HOST_PATH=hynt.us
+set HOST_PROJECT=hynt-web
+set APP_DOMAIN=hynt.us
+set APP_NAME=hynt
+set APP_SHORT_NAME=hynt
+set APP_DESCRIPTION="link shortener"
