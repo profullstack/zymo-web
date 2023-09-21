7c56aa150ef89b8ea9b39efc3c0587f56737e39e
Anthony Ettinger <anthony@chovy.com>
2023-09-21 08:13:20 -0700

    package

--- stores/User.js
+++ stores/User.js
@@ -1,121 +1,126 @@
-import env from "runtime-compat/env";
-import {primary} from "@primate/types";
+import env from 'runtime-compat/env';
+import { primary } from '@primate/types';
 
-export const actions = ({connection: db}) => {
-  return {
-    async me() {
-      const me = await db.info();
-      delete me.password;
-      console.log("me: ", me.email);
-      return me;
-    },
-    async generateEmailVerifyCode(id) {
-      const code = Math.random().toString(36).substr(2, 10);
-      const expiration = new Date(Date.now() + 2 * (60 * 60 * 1000));
+export const actions = ({ connection: db }) => {
+	return {
+		async me() {
+			const me = await db.info();
+			delete me.password;
+			console.log('me: ', me.email);
+			return me;
+		},
+		async generateEmailVerifyCode(id) {
+			const code = Math.random().toString(36).substr(2, 10);
+			const expiration = new Date(Date.now() + 2 * (60 * 60 * 1000));
 
-      const result = (
-        await db.query("UPDATE $id SET verify.email.code = $code, verify.email.status = 'pending', verify.email.expiration = $expiration", {
-          id,
-          code,
-          expiration,
-        })
-      ).pop()
-        .result
-        .pop();
+			const result = (
+				await db.query(
+					"UPDATE $id SET verify.email.code = $code, verify.email.status = 'pending', verify.email.expiration = $expiration",
+					{
+						id,
+						code,
+						expiration
+					}
+				)
+			)
+				.pop()
+				.result.pop();
 
-      console.log('user: ', result);
-      return result;
-    },
-    async generatePhoneVerifyCode(id) {
-      const code = Math.random().toString().substr(2, 6);
-      const expiration = new Date(Date.now() + 2 * (60 * 60 * 1000));
+			console.log('user: ', result);
+			return result;
+		},
+		async generatePhoneVerifyCode(id) {
+			const code = Math.random().toString().substr(2, 6);
+			const expiration = new Date(Date.now() + 2 * (60 * 60 * 1000));
 
-      const result = (
-        await db.query("UPDATE $id SET verify.phone.code = $code, verify.phone.status = 'pending', verify.phone.expiration = $expiration", {
-          id,
-          code,
-          expiration,
-        })
-      ).pop()
-        .result
-        .pop();
+			const result = (
+				await db.query(
+					"UPDATE $id SET verify.phone.code = $code, verify.phone.status = 'pending', verify.phone.expiration = $expiration",
+					{
+						id,
+						code,
+						expiration
+					}
+				)
+			)
+				.pop()
+				.result.pop();
 
-      console.log("user: ", result);
-      return result;
-    },
-    
-    async create(user) {
-      console.log('create:', user);
-      let {email, username, phone, password, password2} = user;
-      const {DB_NS, DB_DB} = env;
+			console.log('user: ', result);
+			return result;
+		},
 
-      console.log('db:', DB_NS, DB_DB)
+		async create(user) {
+			console.log('create:', user);
+			let { email, username, phone, password, password2 } = user;
+			const { DB_NS, DB_DB } = env;
 
-      if (password !== password2) {
-        throw new Error("Passwords do not match");
-      }
+			console.log('db:', DB_NS, DB_DB);
 
-      username = username.replace(/[^a-zA-Z0-9]+/g, "");
-      console.log('user:', user, DB_NS, DB_DB);
+			if (password !== password2) {
+				throw new Error('Passwords do not match');
+			}
 
-      try {
-        const token = await db.signup({
-          NS: DB_NS,
-          DB: DB_DB,
-          SC: "allusers",
-          email,
-          username,
-          password,
-//          createdAt: new Date().toISOString(),
- //         updatedAt: new Date().toISOString(),
-        });
+			username = username.replace(/[^a-zA-Z0-9]+/g, '');
+			console.log('user:', user, DB_NS, DB_DB);
 
-        console.log("token: ", token);
-        return token;
-      } catch(err) {
-        console.error(err);
-        throw err;
-      }
-    },
+			try {
+				const token = await db.signup({
+					NS: DB_NS,
+					DB: DB_DB,
+					SC: 'allusers',
+					email,
+					phone,
+					username,
+					password,
+					createdAt: new Date().toISOString(),
+					updatedAt: new Date().toISOString()
+				});
 
-    async signin(user) {
-      const {email, password} = user;
-      const {DB_NS, DB_DB} = env;
+				console.log('token: ', token);
+				return token;
+			} catch (err) {
+				console.error(err);
+				throw err;
+			}
+		},
 
-  		console.log(user);
+		async signin(user) {
+			const { email, password } = user;
+			const { DB_NS, DB_DB } = env;
 
-      try {
-        const token = await db.signin({
-          NS: DB_NS,
-          DB: DB_DB,
-          SC: "allusers",
-          email,
-          password,
-        });
+			console.log(user);
 
-        console.log("token: ", token);
+			try {
+				const token = await db.signin({
+					NS: DB_NS,
+					DB: DB_DB,
+					SC: 'allusers',
+					email,
+					password
+				});
 
-        const me = await this.me();
-        const {id} = me;
-        const now = new Date();
+				console.log('token: ', token);
 
-        await db.query("UPDATE $id SET loggedInAt = $now", {
-          id,
-          now,
-        });
+				const me = await this.me();
+				const { id } = me;
+				const now = new Date();
 
-        return token;
-      } catch (err) {
-        console.error(err);
-        throw err;
-      }
-    },
-    async logout(session) {
+				await db.query('UPDATE $id SET loggedInAt = $now', {
+					id,
+					now
+				});
 
-    }
-  };
+				return token;
+			} catch (err) {
+				console.error(err);
+				throw err;
+			}
+		},
+		async logout(session) {}
+	};
 };
 
 export default {
-  id: primary,
+	id: primary
 };
