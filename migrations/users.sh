512cfcda4a2683d92b5cb2a66b682968eb2fc1f0
Anthony Ettinger <anthony@chovy.com>
2023-09-24 04:34:05 -0700

    fix surrealdb

--- migrations/users.sh
+++ migrations/users.sh
@@ -11,29 +11,29 @@ DATA="DEFINE TABLE user SCHEMAFULL
     FOR update, delete WHERE id = \$auth.id,
     FOR create NONE;
 DEFINE FIELD email ON user TYPE string;
-DEFINE FIELD phone ON user TYPE string;
+DEFINE FIELD phone ON user TYPE option<string>;
 DEFINE FIELD username ON user TYPE string;
 DEFINE FIELD createdAt ON user TYPE datetime;
 DEFINE FIELD updatedAt ON user TYPE datetime;
-DEFINE FIELD loggedInAt ON user TYPE datetime;
+DEFINE FIELD loggedInAt ON user TYPE option<datetime>;
 DEFINE FIELD password ON user TYPE string;
-DEFINE FIELD settings ON user TYPE object;
-DEFINE FIELD settings.location ON user TYPE geometry<point>;
-DEFINE FIELD settings.timezone ON user TYPE string;
-DEFINE FIELD settings.languages ON user TYPE array;
-DEFINE FIELD settings.languages.* ON user TYPE string;
-DEFINE FIELD settings.offset ON user TYPE string;
--- DEFINE FIELD settings.apikeys ON user TYPE array;
--- DEFINE FIELD settings.apikeys.* ON user TYPE object;
-DEFINE FIELD verify ON user TYPE object;
-DEFINE FIELD verify.email ON user TYPE object;
-DEFINE FIELD verify.email.code ON user TYPE string;
-DEFINE FIELD verify.email.expiration ON user TYPE datetime;
-DEFINE FIELD verify.email.status ON user TYPE string;
-DEFINE FIELD verify.phone ON user TYPE object;
-DEFINE FIELD verify.phone.code ON user TYPE string;
-DEFINE FIELD verify.phone.expiration ON user TYPE datetime;
-DEFINE FIELD verify.phone.status ON user TYPE string;
+DEFINE FIELD settings ON user TYPE option<object>;
+DEFINE FIELD settings.location ON user TYPE option<geometry<point>>;
+DEFINE FIELD settings.timezone ON user TYPE option<string>;
+DEFINE FIELD settings.languages ON user TYPE option<array>;
+DEFINE FIELD settings.languages.* ON user TYPE option<string>;
+DEFINE FIELD settings.offset ON user TYPE option<string>;
+DEFINE FIELD settings.apikeys ON user TYPE option<array>;
+DEFINE FIELD settings.apikeys.* ON user TYPE option<object>;
+DEFINE FIELD verify ON user TYPE option<object>;
+DEFINE FIELD verify.email ON user TYPE option<object>;
+DEFINE FIELD verify.email.code ON user TYPE option<string>;
+DEFINE FIELD verify.email.expiration ON user TYPE option<datetime>;
+DEFINE FIELD verify.email.status ON user TYPE option<string>;
+DEFINE FIELD verify.phone ON user TYPE option<object>;
+DEFINE FIELD verify.phone.code ON user TYPE option<string>;
+DEFINE FIELD verify.phone.expiration ON user TYPE option<datetime>;
+DEFINE FIELD verify.phone.status ON user TYPE option<string>;
 DEFINE INDEX idx_email ON user COLUMNS email UNIQUE;
 DEFINE INDEX idx_username ON user COLUMNS username UNIQUE;
 "
@@ -50,14 +50,14 @@ curl -k -L -s --compressed POST \
 DATA="DEFINE SCOPE allusers
   -- the JWT session will be valid for 14 days
   SESSION 14d
-	SIGNUP ( CREATE user SET username = \$username, email = \$email, password = crypto::argon2::generate(\$password) )
 	SIGNIN ( SELECT * FROM user WHERE email = \$email AND crypto::argon2::compare(password, \$password) )
+  SIGNUP ( CREATE user SET username = \$username, email = \$email, phone = \$phone, password = crypto::argon2::generate(\$password), createdAt = \$createdAt, updatedAt = \$updatedAt )
 
   -- The optional SIGNUP clause will be run when calling the signup method for this scope
   -- It is designed to create or add a new record to the database.
   -- If set, it needs to return a record or a record id
   -- The variables can be passed in to the signin method
-  -- SIGNUP ( CREATE user SET username = \$username, email = \$email, phone = \$phone, password = crypto::argon2::generate(\$password), createdAt = \$createdAt, updatedAt = \$updatedAt )
+  --	SIGNUP ( CREATE user SET username = \$username, email = \$email, password = crypto::argon2::generate(\$password), phone = \$phone )
   -- The optional SIGNIN clause will be run when calling the signin method for this scope
   -- It is designed to check if a record exists in the database.
   -- If set, it needs to return a record or a record id
