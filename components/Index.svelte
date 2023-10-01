3cd84868b17fa32e600f2c3bfa23ad42fbb389fb
Anthony Ettinger <anthony@chovy.com>
2023-10-01 09:58:43 -0700

    app description

--- components/Index.svelte
+++ components/Index.svelte
@@ -1,5 +1,7 @@
 <script>
-    export let APP_NAME;
+    export let APP_NAME, APP_DESCRIPTION;
 </script>
 
 <h1>Welcome to {APP_NAME}!</h1>
+
+<p>{APP_DESCRIPTION}</p>
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ import { view } from 'primate';
 export default {
 	get() {
 		const { env } = process;
-		const { APP_NAME } = env;
+		const { APP_NAME, APP_DESCRIPTION } = env;
 
-		return view('Index.svelte', { APP_NAME });
+		return view('Index.svelte', { APP_NAME, APP_DESCRIPTION });
 	}
 };
