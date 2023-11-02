dec80249894eed5a048cb93c04d9da2707c4283f
Anthony Ettinger <anthony@chovy.com>
2023-11-01 18:20:27 -0700

    mailgun

--- stores/external/Mailgun.js
+++ stores/external/Mailgun.js
@@ -1,30 +1,30 @@
-import env from "runtime-compat/env";
-import { Base64 } from "runtime-compat/string";
-import { MediaType } from "runtime-compat/http";
+import env from 'runtime-compat/env';
+import { Base64 } from 'runtime-compat/string';
+import { MediaType } from 'runtime-compat/http';
 
 export const ambiguous = true;
 
-const { MAILGUN_DOMAIN: domain, MAILGUN_KEY: key, FROM_EMAIL: from } = env;
+const { MAILGUN_DOMAIN: domain, MAILGUN_KEY: key } = env;
 const resource = `https://api.mailgun.net/v3/${domain}/messages`;
 const options = {
-  method: "POST",
-  headers: {
-    "Content-Type": MediaType.APPLICATION_FORM_URLENCODED,
-    Authorization: `Basic ${Base64.encode(`api:${key}`)}`,
-  },
+	method: 'POST',
+	headers: {
+		'Content-Type': MediaType.APPLICATION_FORM_URLENCODED,
+		Authorization: `Basic ${Base64.encode(`api:${key}`)}`
+	}
 };
-const body = ({ subject, to, text }) =>
-  `from=${from}&to=${encodeURIComponent(to)}&subject=${subject}&text=${text}`;
+const body = ({ subject, to, from, text }) =>
+	`from=${from}&to=${to}&subject=${subject}&text=${text}`;
 
 export const actions = () => {
-  return {
-    async send(mail) {
-      try {
-        await fetch(resource, { ...options, body: body(mail) });
-        console.log(`sent email to ${mail.to}`);
-      } catch (error) {
-        console.warn(error);
-      }
-    },
-  };
+	return {
+		async send(mail) {
+			try {
+				const res = await fetch(resource, { ...options, body: body(mail) });
+				console.log(`sent email to ${mail.to}`, await res.text());
+			} catch (error) {
+				console.error(error);
+			}
+		}
+	};
 };
