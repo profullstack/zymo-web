a67430bd0b10290d06e5678455953d1c4b8f8813
terrablue <102580937+terrablue@users.noreply.github.com>
2023-11-03 02:06:00 +0200

    fix issues

--- primate.config.js
+++ primate.config.js
@@ -1,29 +1,29 @@
-import { svelte } from '@primate/frontend';
-import store from '@primate/store';
-import { surrealdb } from '@primate/store';
-import types from '@primate/types';
-import session from '@primate/session';
-import ws from '@primate/ws';
-import esbuild from '@primate/esbuild';
+import { svelte } from "@primate/frontend";
+import store from "@primate/store";
+import { surrealdb } from "@primate/store";
+import types from "@primate/types";
+import session from "@primate/session";
+import ws from "@primate/ws";
+import esbuild from "@primate/esbuild";
 // import liveview from '@primate/liveview';
-import { config } from 'dotenv-flow';
-import { Logger } from 'primate';
+import { config } from "dotenv-flow";
+import { Logger } from "primate";
 
 config();
 
 const {
-	PORT: port,
-	DB_USER: user,
-	DB_PASS: pass,
-	DB_HOST: host,
-	DB_NS: ns,
-	DB_DB: db,
-	DB_PORT: db_port,
-	APP_DOMAIN,
-	GOOGLE_ANALYTICS_ID,
-	APP_NAME,
-	APP_SHORT_NAME,
-	APP_DESCRIPTION
+  PORT: port,
+  DB_USER: username,
+  DB_PASS: password,
+  DB_HOST: host,
+  DB_NS: namespace,
+  DB_DB: database,
+  DB_PORT: db_port,
+  APP_DOMAIN,
+  GOOGLE_ANALYTICS_ID,
+  APP_NAME,
+  APP_SHORT_NAME,
+  APP_DESCRIPTION,
 } = process.env;
 
 console.log(host, db_port);
@@ -32,47 +32,47 @@ export default {
 	logger: {
 		// show all logs
 		level: Logger.Info,
-		trace: true
+		trace: true,
 	},
 	http: {
 		port,
 		csp: {
-			'script-src': "'unsafe-inline' 'self' plausible.io googletagmanager.com",
-			'style-src': "'self' *.tile.openstreetmap.org unpkg.com",
-			'img-src': "'self' *.tile.openstreetmap.org unpkg.com"
-		}
+			"script-src": "'unsafe-inline' 'self' plausible.io googletagmanager.com",
+			"style-src": "'self' *.tile.openstreetmap.org unpkg.com",
+			"img-src": "'self' *.tile.openstreetmap.org unpkg.com",
+		},
 	},
 	build: {
 		transform: {
-			paths: ['pages/app.html', 'static/manifest.json'],
+			paths: ["pages/app.html", "static/manifest.json"],
 			mapper: (contents) =>
-				contents
-					.replaceAll('APP_DOMAIN', APP_DOMAIN)
-					.replaceAll('GOOGLE_ANALYTICS_ID', GOOGLE_ANALYTICS_ID)
-					.replaceAll('APP_NAME', APP_NAME)
-					.replaceAll('APP_SHORT_NAME', APP_SHORT_NAME)
-					.replaceAll('APP_DESCRIPTION', APP_DESCRIPTION)
-		}
+			  contents
+			    .replaceAll("APP_DOMAIN", APP_DOMAIN)
+			    .replaceAll("GOOGLE_ANALYTICS_ID", GOOGLE_ANALYTICS_ID)
+			    .replaceAll("APP_NAME", APP_NAME)
+			    .replaceAll("APP_SHORT_NAME", APP_SHORT_NAME)
+			    .replaceAll("APP_DESCRIPTION", APP_DESCRIPTION),
+		},
 	},
 	modules: [
-		svelte(),
-		// liveview(),
-		store({
+	  svelte(),
+	  // liveview(),
+	  store({
 			strict: true,
 			driver: surrealdb({
 				host,
 				port: db_port,
-				ns,
-				db,
-				user,
-				pass
-			})
+				namespace,
+				database,
+				username,
+				password,
+			}),
 		}),
-		types(),
-		session(),
-		ws(),
-		esbuild({
-			ignores: ['woff', 'ttf', 'png', 'svg']
-		})
-	]
+	  types(),
+	  session(),
+	  ws(),
+	  esbuild({
+			ignores: ["woff", "ttf", "png", "svg"],
+		}),
+	],
 };
