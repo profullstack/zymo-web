6e47d22c14badef40c8f5458ba4208a16673bba5
Anthony Ettinger <anthony@chovy.com>
2023-11-14 17:14:19 -0800

    esbuld

--- primate.config.js
+++ primate.config.js
@@ -4,7 +4,7 @@ import { surrealdb } from "@primate/store";
 import types from "@primate/types";
 import session from "@primate/session";
 import ws from "@primate/ws";
-import esbuild from "@primate/build";
+import { esbuild } from '@primate/build';
 // import liveview from '@primate/liveview';
 import { config } from "dotenv-flow";
 import { Logger } from "primate";
