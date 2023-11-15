b4ac5848636ce3747a29740915ed5465273e88b7
Anthony Ettinger <anthony@chovy.com>
2023-11-14 23:07:22 -0800

    migrations

--- migrations/users.sh
+++ migrations/users.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
 # See: https://gist.githubusercontent.com/koakh/fbbc37cde630bedcf57acfd4d6a6956b/raw/ec0d25ce0dba9b729f4e8d379149e4b99cced7bc/gistfile1.txt
 
@@ -49,24 +49,9 @@ curl -k -L -s --compressed POST \
 
 
 DATA="DEFINE SCOPE allusers
-  -- the JWT session will be valid for 14 days
   SESSION 14d
-  -- SIGNIN ( SELECT * FROM user WHERE email = \$email AND crypto::argon2::compare(password, \$password) )
   SIGNIN ( SELECT * FROM user WHERE email = \$email AND crypto::argon2::compare(password, \$password) )
   SIGNUP ( CREATE user SET username = \$username, email = \$email, phone = \$phone, phonePrefix = \$phonePrefix, firstName = \$firstName, lastName = \$lastName, password = crypto::argon2::generate(\$password), createdAt = \$createdAt, updatedAt = \$updatedAt )
-
-  -- The optional SIGNUP clause will be run when calling the signup method for this scope
-  -- It is designed to create or add a new record to the database.
-  -- If set, it needs to return a record or a record id
-  -- The variables can be passed in to the signin method
-  --	SIGNUP ( CREATE user SET username = \$username, email = \$email, password = crypto::argon2::generate(\$password), phone = \$phone )
-  -- The optional SIGNIN clause will be run when calling the signin method for this scope
-  -- It is designed to check if a record exists in the database.
-  -- If set, it needs to return a record or a record id
-  -- The variables can be passed in to the signin method
-  -- SIGNIN ( SELECT * FROM user WHERE email = \$email AND crypto::argon2::compare(password, \$password) )
-  -- SIGNIN ( SELECT * FROM user WHERE settings.apiKeys.key = \$apikey OR (email = \$email AND crypto::argon2::compare(password, \$password)) )
-  -- this optional clause will be run when calling the signup method for this scope
 ;"
 
 curl -k -L -s --compressed POST \
@@ -78,7 +63,6 @@ curl -k -L -s --compressed POST \
 	${DB_SQL_URL}
 
 DATA="DEFINE SCOPE apiusers
-  -- the JWT session will be valid for 14 days
   SESSION 14d
   SIGNIN ( SELECT *, (SELECT VALUE apikey FROM apikeys WHERE apikeys.createdBy = id) as apikeys FROM user WHERE apikeys contains \$apikey );
 "
@@ -93,9 +77,10 @@ curl -k -L -s --compressed POST \
 
 
 DATA="DEFINE SCOPE nostrusers
-  -- the JWT session will be valid for 14 days
   SESSION 14d
-  SIGNIN ( SELECT *, (SELECT * FROM nostrusers) as nostrusers FROM user WHERE nostrpub.id = \$nostrPub )
+--    SIGNUP ( CREATE nostrusers SET username = \$username, email = \$email, phone = \$phone, phonePrefix = \$phonePrefix, firstName = \$firstName, lastName = \$lastName, password = crypto::argon2::generate(\$password), createdAt = \$createdAt, updatedAt = \$updatedAt )
+
+  SIGNIN ( SELECT *, (SELECT * FROM nostrusers) as nostrusers FROM user WHERE nostrpub.id contains \$nostrPub )
 ;"
 
 curl -k -L -s --compressed POST \
