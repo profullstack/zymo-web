099fea9d91d45c472db337ccc845af07dde990ce
Anthony Ettinger <anthony@chovy.com>
2023-11-18 09:34:27 -0800

    Auto stash before revert of "keywords"

--- bin/backup.sh
+++ bin/backup.sh
@@ -6,21 +6,22 @@
 
 path=$HOME
 now=$(date +%Y-%m-%d_%H.%M.%S)
+site=hynt.us
 
 backup() {
-	mkdir -p ${path}/backups/${now}/mongo ${path}/backups/${now}/mysql
+	mkdir -p ${path}/backups/${site}_${now}/mongo ${path}/backups/${site}_${now}/mysql
 	# dump-mongo
-	tar czfh ${path}/backups/${now}/www.tgz ${path}/www
-	scp -r ${path}/backups/${now}/ profullstack:~/backups/
+	tar czfh ${path}/backups/${site}_${now}/www.tgz ${path}/www
+	scp -r ${path}/backups/${site}_${now}/ profullstack:~/backups/
 }
 
 dump-mysql() {
- 	sudo mysqldump --all-databases --add-drop-table --all-databases --single-transaction --set-gtid-purged=OFF --user=root -h 127.0.0.1 -P 33061 > ${path}/backups/${now}/mysql/dump.sql
+ 	sudo mysqldump --all-databases --add-drop-table --all-databases --single-transaction --set-gtid-purged=OFF --user=root -h 127.0.0.1 -P 33061 > ${path}/backups/${site}_${now}/mysql/dump.sql
 }
 
 dump-mongo(){
-	mongodump --host="localhost" --port=27017 -d avenasea --forceTableScan -o ${path}/backups/${now}/mongo/avenasea
-	mongodump --host="localhost" --port=27017 -d grazily --forceTableScan -o ${path}/backups/${now}/mongo/grazily
+	mongodump --host="localhost" --port=27017 -d avenasea --forceTableScan -o ${path}/backups/${site}_${now}/mongo/avenasea
+	mongodump --host="localhost" --port=27017 -d grazily --forceTableScan -o ${path}/backups/${site}_${now}/mongo/grazily
 }
 
 backup
