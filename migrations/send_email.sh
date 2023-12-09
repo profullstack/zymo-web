44ac4b904338eadba62fa1342194c14a08a6d2ea
Anthony Ettinger <anthony@chovy.com>
2023-12-09 02:19:43 -0800

    spinner send_email

---
+++ migrations/send_email.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+. .env
+. .env.local
+
+DATA="DEFINE TABLE send_email SCHEMALESS
+    PERMISSIONS NONE DROP;
+
+DEFINE FIELD from_name ON TABLE send_email TYPE string VALUE \$value OR 'Kards Social';
+DEFINE FIELD from_email ON TABLE send_email TYPE string VALUE \$value OR 'hello@globalthreat.info' ASSERT is::email(\$value);
+DEFINE FIELD to_name ON TABLE send_email TYPE string;
+DEFINE FIELD to_email ON TABLE send_email TYPE string ASSERT is::email(\$value);
+DEFINE FIELD subject ON TABLE send_email TYPE string;
+DEFINE FIELD content ON TABLE send_email TYPE array ASSERT array::len(\$value) > 0;
+
+# Always send the email :D    (1=1)
+DEFINE EVENT email ON TABLE send_email WHEN 1=1 THEN {
+    LET \$bearer_token = SELECT value FROM environment:sendgrid_key;
+
+    SELECT * FROM http::post('https://api.sendgrid.com/v3/mail/send', {
+        personalizations: [
+            {
+                from: {
+                    email: \$after.from_email,
+                    name: \$after.from_name
+                },
+                to: [{
+                    email: \$after.to_email,
+                    name: \$after.to_name
+                }],
+                subject: \$after.subject,
+            }
+        ],
+        from: {
+            email: \$after.from_email,
+            name: \$after.from_name
+        },
+        subject: \$after.subject,
+        content: \$after.content
+    }, {
+        Authorization: \"Bearer \" + \$bearer_token.value,
+        \"Content-Type\": \"application/json\",
+    })
+};
+"
+# 	-- FOR select WHERE \$scope = \"allusers\" -- limit to only users in db
+
+curl -k -L -s --compressed POST \
+	--header "Accept: application/json" \
+	--header "NS: ${DB_NS}" \
+	--header "DB: ${DB_DB}" \
+	--user "root:root" \
+	--data "${DATA}" \
+	${DB_SQL_URL}
