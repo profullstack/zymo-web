3f80b5628b9b7888cf58577023e7fd4124e43287
Anthony Ettinger <anthony@chovy.com>
2024-01-12 00:27:53 -0800

    fix surreal shit

--- bin/post-deploy.sh
+++ bin/post-deploy.sh
@@ -20,24 +20,21 @@ if [ -d "$HOME/www/${name}/${project}" ]; then
     npm -v
     npm i
 		# curl --proto '=https' --tlsv1.2 -sSf https://install.surrealdb.com | sh -s -- --nightl
+    sudo systemctl stop ${META_SERVICE}
+    sudo systemctl stop surrealdb
     surreal upgrade --nightly
+    sudo /etc/init.d/nginx reload
+    sudo systemctl daemon-reload
+    sudo systemctl start ${META_SERVICE}
+    sudo systemctl start surrealdb
     # run migrations
     ./migrations/users.sh
-    # ./migrations/deals.sh
     # ./migrations/comments.sh
     ./migrations/links.sh
     ./migrations/apikeys.sh
     ./migrations/nostrusers.sh
-    # ./migrations/applications.sh
-    # ./migrations/services.sh
-    # ./migrations/inquiries.sh
 
-    # bump app
-    sudo /etc/init.d/nginx reload
-    sudo systemctl daemon-reload
-    sudo systemctl restart ${META_SERVICE}
-    # sudo systemctl restart scraper-all
-else
+  else
     echo "One or both directories do not exist"
 fi
 
---
+++ bin/surrealdb-start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#cd "$(dirname "$0")/.."
+. $HOME/.bashrc
+. "$NVM_DIR/nvm.sh" && nvm use v20
+. .env
+. .env.local
+
+
+npm run db:start
+
