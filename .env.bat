ca7745baa6db1573626819678399869dda38bad8
Anthony Ettinger <anthony@chovy.com>
2024-02-23 06:56:08 -0800

    local examples and .bat

--- .env.bat
+++ .env.bat
@@ -1,8 +1,3 @@
 set HOST_USER=ubuntu
-set HOST_DOMAIN=profullstack
-set HOST_PATH=hynt.us
-set HOST_PROJECT=hynt-web
-set APP_DOMAIN=hynt.us
-set APP_NAME=hynt
-set APP_SHORT_NAME=hynt
-set APP_DESCRIPTION="link shortener"
+set HOST_DOMAIN=profullstacknew
+
