a2ccd048ca021665ca87fcf9b112bbe57d8108e7
Anthony Ettinger <anthony@chovy.com>
2024-06-15 06:48:02 -0700

    fix metatags

--- components/MetaTags.svelte
+++ components/MetaTags.svelte
@@ -1,23 +1,22 @@
 <script>
-	import { page } from '$app/stores';
-
 	let defaultMetaData = {
 		title: 'APP_NAME',
-		description:
-			'APP_DESCRIPTION',
+		description: 'APP_DESCRIPTION',
 		path: ``,
 		imagePath: `/images/meta-banner.png`
 	};
 	let currentMetaData = defaultMetaData;
+	export let title;
+	export let description;
+	export let path;
+	export let imagePath;
 
-	page.subscribe((value) => {
-		currentMetaData = {
-			title: value.data?.meta?.title || defaultMetaData.title,
-			description: value.data?.meta?.description || defaultMetaData.description,
-			path: value.data?.meta?.path || defaultMetaData.path,
-			imagePath: value.data?.meta?.imagePath || defaultMetaData.imagePath
-		};
-	});
+	currentMetaData = {
+		title: title || defaultMetaData.title,
+		description: description || defaultMetaData.description,
+		path: path || defaultMetaData.path,
+		imagePath: imagePath || defaultMetaData.imagePath
+	};
 </script>
 
 <meta name="description" content={currentMetaData.description} />
@@ -26,22 +25,13 @@
 <meta property="og:title" content={currentMetaData.title} />
 <meta property="og:description" content={currentMetaData.description} />
 <meta property="og:type" content="website" />
-<meta
-	property="og:url"
-	content={`https://${import.meta.env.VITE_META_DOMAIN}${currentMetaData.path}`}
-/>
-<meta
-	property="og:image"
-	content={`https://${import.meta.env.VITE_META_DOMAIN}${currentMetaData.imagePath}`}
-/>
+<meta property="og:url" content={`https://APP_DOMAIN${currentMetaData.path}`} />
+<meta property="og:image" content={`https://APP_DOMAIN${currentMetaData.imagePath}`} />
 
 <!-- Twitter Card Meta Tags -->
 <meta property="twitter:card" content="summary_large_image" />
 <meta property="twitter:title" content={currentMetaData.title} />
-<meta property="twitter:url" content={`https://${import.meta.env.VITE_META_DOMAIN}`} />
-<meta property="twitter:domain" content={import.meta.env.VITE_META_DOMAIN} />
+<meta property="twitter:url" content={`https://APP_DOMAIN`} />
+<meta property="twitter:domain" content="APP_DOMAIN" />
 <meta property="twitter:description" content={currentMetaData.description} />
-<meta
-	property="twitter:image"
-	content={`https://${import.meta.env.VITE_META_DOMAIN}${currentMetaData.imagePath}`}
-/>
+<meta property="twitter:image" content={`https://APP_DOMAIN${currentMetaData.imagePath}`} />
