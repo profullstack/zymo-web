856f57362951e4091072338acedfe2e69f051f52
Anthony Ettinger <anthony@chovy.com>
2024-06-18 07:29:17 -0700

    wise

--- components/AffiliateDashboard.svelte
+++ components/AffiliateDashboard.svelte
@@ -2,7 +2,6 @@
 	export let affiliate,
 		referrals = [];
 	export let APP_DOMAIN, AFFILIATE_COMMISSION_PERCENT;
-
 </script>
 
 <div>
@@ -10,7 +9,7 @@
 	{#if affiliate}
 		<div style="padding: 10px;border: 1px solid;border-radius: 10px;">
 			<h4>Referrals: {referrals.length}</h4>
-			<h4>Commission Percentage: {AFFILIATE_COMMISSION_PERCENT} % </h4>
+			<h4>Commission Percentage: {AFFILIATE_COMMISSION_PERCENT} %</h4>
 			<h4>Commissions Earned: $ {affiliate.commissions}</h4>
 			<h4>Referral Code: {affiliate.referralCode}</h4>
 			<h4>Referral Link: http://{APP_DOMAIN}/referral/{affiliate.referralCode}</h4>
@@ -20,4 +19,10 @@
 			<button>Become an Affiliate</button>
 		</form>
 	{/if}
+
+	<p>
+		For payouts, we recommend using <a href="https://wise.com/invite/dic/anthonye828"
+			>wise.com</a
+		> as they support International payments via ACH.
+	</p>
 </div>
