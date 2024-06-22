e2aae2c63cc02be7cf82349b412fa9e7b9f443b1
Anthony Ettinger <anthony@chovy.com>
2024-06-21 20:03:01 -0700

    fix fs.FlatFile

--- stores/Countries.js
+++ stores/Countries.js
@@ -1,6 +1,11 @@
 import * as FS from 'rcompat/fs';
 
-const countries = await FS.File.join(import.meta.dirname, '..', 'static', 'country.json').json();
+const countries = await FS.FlatFile.join(
+	import.meta.dirname,
+	'..',
+	'static',
+	'country.json'
+).json();
 
 export const ambiguous = true;
 
