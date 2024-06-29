83f2e9e1d9e05d8069222638bbea02b006cb1280
Daniel-Ozori <ozoridaniel0@gmail.com>
2024-06-29 13:41:23 +0100

    Add waitlist

--- components/Index.svelte
+++ components/Index.svelte
@@ -1,5 +1,37 @@
 <script>
 	export let APP_NAME, APP_DESCRIPTION;
+
+	let waitlistEmail = '';
+
+	function isEmail(email) {
+		var emailFormat = /^[a-zA-Z0-9_.+]+(?<!^[0-9]*)@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+		if (email !== '' && email.match(emailFormat)) { return true; }
+		
+		return false;
+	}
+	async function joinWaitlist(e) {
+		e.preventDefault()
+		try{
+			if(!waitlistEmail || !isEmail(waitlistEmail)){
+				alert("Please provide a valid email address");
+				return
+			}
+
+			const response = await fetch('/waitlist', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({ email: waitlistEmail })
+			});
+
+			if(response.ok) {
+				alert("Successfully subscribed to waitlist")
+			}else{
+				alert("Email already exists");
+			}
+		}catch(e) {	}
+	}
 </script>
 
 <svelte:head>
@@ -9,3 +41,11 @@
 <h1>Welcome to {APP_NAME}!</h1>
 
 <p>{APP_DESCRIPTION}</p>
+
+<h3>Join our waitlist</h3>
+
+<form>
+	<input bind:value={waitlistEmail} placeholder="Email address"/>
+	<br/><br/>
+	<button on:click={joinWaitlist}> Join now </button>
+</form>
