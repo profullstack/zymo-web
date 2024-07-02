17b85c8c607f0132dd3d69741106bef486021dd8
Daniel-Ozori <ozoridaniel0@gmail.com>
2024-07-02 19:47:49 +0100

    Remove amount field

--- migrations/payouts.sh
+++ migrations/payouts.sh
@@ -9,7 +9,7 @@ DATA="DEFINE TABLE payouts SCHEMALESS
     FOR update, create WHERE \$scope = \"allusers\" OR \$scope = \"apiusers\" OR \$scope = \"allnostrusers\",
 	FOR delete WHERE userId = \$auth.id;
 DEFINE FIELD method ON payouts TYPE string;
-DEFINE FIELD amount ON payouts TYPE number;
+DEFINE FIELD amount ON payouts TYPE option<number>;
 DEFINE FIELD status ON payouts TYPE string;
 DEFINE FIELD details ON payouts TYPE object;
 DEFINE INDEX idx_userId ON payouts COLUMNS userId;
