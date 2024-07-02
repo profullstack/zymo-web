33cf8c5d0119243079e623133cfe6301e86e5260
Anthony Ettinger <anthony@chovy.com>
2024-07-02 04:32:15 -0700

    add public affilate page

---
+++ components/affiliate/Join.svelte
@@ -0,0 +1,16 @@
+<h1>Become a Partner in Success!</h1>
+
+<p class="hero">Earn up to $4,000 a Month in Commissions!</p>
+
+<p>
+	Ready to start earning? Simply <a href="/affiliate"
+		>sign up and access your personal affiliate dashboard</a
+	>. It's easy to generate your unique affiliate links right from there.
+</p>
+
+<p>Track every click, view your earnings, and monitor sales all from one convenient dashboard.</p>
+
+<p>
+	Plus, our affiliate cookies last for two years—giving you plenty of time to promote and earn.
+	Start your promotional journey today!
+</p>
