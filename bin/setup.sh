d9373cc1fcccf2b5144734fdd2a672e2490c2889
Anthony Ettinger <anthony@chovy.com>
2024-07-14 08:14:40 -0700

    fix setup

--- bin/setup.sh
+++ bin/setup.sh
@@ -18,18 +18,19 @@ NGINX_CONF="$TARGET_PATH/etc/$DOMAIN_NAME.conf"
 SYSTEMD_SERVICE="$TARGET_PATH/etc/$PROJECT_NAME.service"
 NGINX_SYMLINK="/etc/nginx/sites-available/$DOMAIN_NAME.com.conf"
 SYSTEMD_SYMLINK="/etc/systemd/system/$PROJECT_NAME.service"
+user=$HOST_USER
 
 # Create the directory if it does not exist
-ssh ubuntu@profullstacknew "mkdir -p $TARGET_PATH"
+ssh $user@$HOST_DOMAIN "mkdir -p $TARGET_PATH"
 
 # Symlink the Nginx configuration
-ssh ubuntu@profullstacknew "if [ -f $NGINX_CONF ]; then sudo ln -sf $NGINX_CONF $NGINX_SYMLINK && sudo ln -sf $NGINX_SYMLINK /etc/nginx/sites-enabled/; else echo 'Nginx configuration file not found at $NGINX_CONF'; exit 1; fi"
+ssh $user@$HOST_DOMAIN "if [ -f $NGINX_CONF ]; then sudo ln -sf $NGINX_CONF $NGINX_SYMLINK && sudo ln -sf $NGINX_SYMLINK /etc/nginx/sites-enabled/; else echo 'Nginx configuration file not found at $NGINX_CONF'; exit 1; fi"
 
 # Symlink the systemd service
-ssh ubuntu@profullstacknewE "if [ -f $SYSTEMD_SERVICE ]; then sudo ln -sf $SYSTEMD_SERVICE $SYSTEMD_SYMLINK; else echo 'Systemd service file not found at $SYSTEMD_SERVICE'; exit 1; fi"
+ssh $user@$HOST_DOMAIN "if [ -f $SYSTEMD_SERVICE ]; then sudo ln -sf $SYSTEMD_SERVICE $SYSTEMD_SYMLINK; else echo 'Systemd service file not found at $SYSTEMD_SERVICE'; exit 1; fi"
 
 # Enable and start the systemd service
-ssh ubuntu@profullstacknew "sudo systemctl enable $(basename $SYSTEMD_SERVICE) --now"
+ssh $user@$HOST_DOMAIN "sudo systemctl enable $(basename $SYSTEMD_SERVICE) --now"
 
 echo "Setup completed successfully!"
 
