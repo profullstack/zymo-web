159c2ae9f0beb4fc16a8b1f252cab13d22df120f
Anthony Ettinger <anthony@chovy.com>
2024-07-19 17:26:24 -0700

    fix meta tags.

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,6 @@ import { view } from 'primate';
 
 export default {
 	get() {
-		const { env } = process;
-		const { APP_NAME, APP_DESCRIPTION } = env;
-
-		return view('Index.svelte', { APP_NAME, APP_DESCRIPTION });
+		return view('Index.svelte');
 	}
 };
