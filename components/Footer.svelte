0552bab9eacf0cf9a4469448899200d2c702513b
Anthony Ettinger <anthony@chovy.com>
2024-08-01 19:45:52 -0700

    footer

---
+++ components/Footer.svelte
@@ -0,0 +1,22 @@
+<footer>
+	<small>
+		<a href="mailto:hello@globalthreat.info">hello@globalthreat.info</a>
+		<a href="/terms">Terms</a>
+		<a href="/privacy">Privacy</a> © 2023-2024
+		<a href="https://profullstack.com">Profullstack, Inc.</a>
+	</small>
+</footer>
+
+<style>
+	footer {
+		display: flex;
+		justify-content: flex-end;
+		align-items: center;
+		border-top: 1px solid #ccc;
+		padding: 1.2rem;
+	}
+
+	footer small {
+		margin-left: 1.2rem;
+	}
+</style>
