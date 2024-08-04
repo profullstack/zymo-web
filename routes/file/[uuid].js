3968fe5a385bfb7aed55d3cd04d7b4822525f1b4
Daniel-Ozori <ozoridaniel0@gmail.com>
2024-08-04 17:40:04 +0100

    Add file upload

---
+++ routes/file/[uuid].js
@@ -0,0 +1,29 @@
+import { Response, Status } from 'primate';
+import file from "@rcompat/fs/file";
+
+export default {
+    async get(request) {
+        const { path, store, session } = request;
+        const { utils: { File } } = store;
+        const uuid = path.get('uuid');
+
+        const fileInfo = await File.getByUUID(uuid);
+
+        if (fileInfo) {
+            const datafile = file(fileInfo.path);
+            if (await datafile.exists()) {
+                if (fileInfo.isPublic || fileInfo.userId === session.get("user")?.id) {
+
+                    return new Response(datafile.stream(), {
+                        status: 200,
+                        headers: {
+                            "Content-Type": fileInfo.type
+                        },
+                    });
+                }
+            }
+        }
+
+        return new Response(null, { status: 404 });
+    },
+};
