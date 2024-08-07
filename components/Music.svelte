995f2dd15374d0922b785a2c6f7dda63b5049504
Anthony Ettinger <anthony@chovy.com>
2024-08-07 01:01:07 -0700

    fix music

--- components/Music.svelte
+++ components/Music.svelte
@@ -1,135 +1,153 @@
 <script>
-	export let music = [];
+  export let music = [];
 
-	// Function to group and sort music files by artist and album
-	function groupAndSortMusic(music) {
-		const grouped = {};
+  // Function to group and sort music files by artist and album
+  function groupAndSortMusic(music) {
+    const grouped = {};
 
-		for (const file of music) {
-			const { mediaInfo } = file;
-			const { url } = file;
-			const { artist, album, songname } = mediaInfo;
+    for (const file of music) {
+      const { mediaInfo } = file;
+      const { url } = file;
+      const { artist, album, songname } = mediaInfo;
 
-			if (!grouped[artist]) {
-				grouped[artist] = {};
-			}
-			if (!grouped[artist][album]) {
-				grouped[artist][album] = [];
-			}
-			grouped[artist][album].push({ songname, url, id: url, playing: false, audio: null });
-		}
+      if (!grouped[artist]) {
+        grouped[artist] = {};
+      }
+      if (!grouped[artist][album]) {
+        grouped[artist][album] = [];
+      }
+      grouped[artist][album].push({ songname, url, id: url, playing: false, audio: null });
+    }
 
-		// Sort artists, albums, and songs
-		const sortedGrouped = {};
-		const sortedArtists = Object.keys(grouped).sort((a, b) => a.localeCompare(b));
+    // Sort artists, albums, and songs
+    const sortedGrouped = {};
+    const sortedArtists = Object.keys(grouped).sort((a, b) => a.localeCompare(b));
 
-		for (const artist of sortedArtists) {
-			sortedGrouped[artist] = {};
-			const sortedAlbums = Object.keys(grouped[artist]).sort((a, b) => a.localeCompare(b));
+    for (const artist of sortedArtists) {
+      sortedGrouped[artist] = {};
+      const sortedAlbums = Object.keys(grouped[artist]).sort((a, b) => a.localeCompare(b));
 
-			for (const album of sortedAlbums) {
-				sortedGrouped[artist][album] = grouped[artist][album].sort((a, b) =>
-					a.songname.localeCompare(b.songname)
-				);
-			}
-		}
+      for (const album of sortedAlbums) {
+        sortedGrouped[artist][album] = grouped[artist][album].sort((a, b) =>
+          a.songname.localeCompare(b.songname)
+        );
+      }
+    }
 
-		return sortedGrouped;
-	}
+    return sortedGrouped;
+  }
 
-	const groupedMusic = groupAndSortMusic(music);
+  let groupedMusic = groupAndSortMusic(music);
 
-	let currentlyPlaying = null;
+  let currentlyPlaying = null;
+  let visibleArtists = new Set();
+  let visibleAlbums = new Set();
 
-	function toggleVisibility(event) {
-		const content = event.target.nextElementSibling;
-		if (content) {
-			content.style.display = content.style.display === 'none' ? 'block' : 'none';
-		}
-	}
+  function toggleVisibility(set, key) {
+    if (set.has(key)) {
+      set.delete(key);
+    } else {
+      set.add(key);
+    }
+    // Force reactivity
+    visibleArtists = new Set(visibleArtists);
+    visibleAlbums = new Set(visibleAlbums);
+  }
 
-	function playSong(song) {
-		if (currentlyPlaying && currentlyPlaying !== song) {
-			currentlyPlaying.audio.pause();
-			currentlyPlaying.playing = false;
-		}
-		song.playing = !song.playing;
-		if (song.playing) {
-			if (!song.audio) {
-				song.audio = new Audio(song.url);
-				song.audio.addEventListener('ended', () => {
-					song.playing = false;
-					currentlyPlaying = null;
-					song.audio = null; // Cleanup the audio object
-				});
-			}
-			song.audio.play();
-			currentlyPlaying = song;
-		} else {
-			song.audio.pause();
-			currentlyPlaying = null;
-		}
-	}
+  function playSong(song) {
+    if (currentlyPlaying && currentlyPlaying !== song) {
+      currentlyPlaying.audio.pause();
+      currentlyPlaying.playing = false;
+      // Force reactivity
+      currentlyPlaying = { ...currentlyPlaying };
+    }
+    song.playing = !song.playing;
+    if (song.playing) {
+      if (!song.audio) {
+        song.audio = new Audio(song.url);
+        song.audio.addEventListener('ended', () => {
+          song.playing = false;
+          currentlyPlaying = null;
+          song.audio = null; // Cleanup the audio object
+          // Force reactivity
+          groupedMusic = { ...groupedMusic };
+        });
+      }
+      song.audio.play();
+      currentlyPlaying = song;
+    } else {
+      song.audio.pause();
+      currentlyPlaying = null;
+    }
+    // Force reactivity
+    groupedMusic = { ...groupedMusic };
+  }
 </script>
 
 {#if Object.keys(groupedMusic).length}
-	<section>
-		{#each Object.entries(groupedMusic) as [artist, albums]}
-			<div class="collapsible" on:click={toggleVisibility}>
-				{artist}
-			</div>
-			<div class="content" style="display: none;">
-				{#each Object.entries(albums) as [album, songs]}
-					<div class="collapsible" on:click={toggleVisibility}>
-						{album}
-					</div>
-					<div class="content" style="display: none;">
-						{#each songs as song (song.id)}
-							<div class="song">
-								<a href={song.url}>{song.songname}</a>
-								<button class="play-button" on:click={() => playSong(song)}>
-									{song.playing ? '❚❚' : '▶'}
-								</button>
-							</div>
-						{/each}
-					</div>
-				{/each}
-			</div>
-		{/each}
-	</section>
+  <section>
+    {#each Object.entries(groupedMusic) as [artist, albums]}
+      <div class="collapsible" on:click={() => toggleVisibility(visibleArtists, artist)}>
+        {artist}
+      </div>
+      {#if visibleArtists.has(artist)}
+        <div class="content">
+          {#each Object.entries(albums) as [album, songs]}
+            <div
+              class="collapsible"
+              on:click={() => toggleVisibility(visibleAlbums, `${artist}-${album}`)}
+            >
+              {album}
+            </div>
+            {#if visibleAlbums.has(`${artist}-${album}`)}
+              <div class="content">
+                {#each songs as song (song.id)}
+                  <div class="song">
+                    <a href={song.url}>{song.songname}</a>
+                    <button class="play-button" on:click={() => playSong(song)}>
+                      {song.playing ? '❚❚' : '▶'}
+                    </button>
+                  </div>
+                {/each}
+              </div>
+            {/if}
+          {/each}
+        </div>
+      {/if}
+    {/each}
+  </section>
 {/if}
 
 <style>
-	.collapsible {
-		cursor: pointer;
-		font-weight: bold;
-	}
+  .collapsible {
+    cursor: pointer;
+    font-weight: bold;
+  }
 
-	.content {
-		display: none;
-		padding-left: 1em;
-	}
+  .content {
+    padding-left: 1em;
+  }
 
-	.song {
-		padding-left: 1em;
-		display: flex;
-		align-items: center;
-	}
+  .song {
+    padding-left: 1em;
+    display: flex;
+    align-items: center;
+  }
 
-	.song a {
-		margin-right: 1em;
-	}
+  .song a {
+    margin-right: 1em;
+  }
 
-	.play-button {
-		background: none;
-		border: none;
-		font-size: 1em;
-		cursor: pointer;
-		margin-left: 0.5em;
-		vertical-align: middle;
-	}
+  .play-button {
+    background: none;
+    border: none;
+    font-size: 1em;
+    cursor: pointer;
+    margin-left: 0.5em;
+    vertical-align: middle;
+  }
 
-	.play-button:focus {
-		outline: none;
-	}
+  .play-button:focus {
+    outline: none;
+  }
 </style>
