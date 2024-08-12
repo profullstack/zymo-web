8c282bbe2e9d3682682d2f8098fd2398b95f6daa
Daniel-Ozori <ozoridaniel0@gmail.com>
2024-08-12 18:18:21 +0100

    Update EPG UI

--- components/m3u/EPG.svelte
+++ components/m3u/EPG.svelte
@@ -5,8 +5,7 @@
 	export let m3u = {};
 
 	let currentTime = new Date();
-	let endTime = new Date(currentTime);
-	endTime.setHours(currentTime.getHours() + 24);
+	let endTime = new Date(currentTime).setHours(currentTime.getHours() + 24);
 
 	let filterText = ''; // Reactive variable for filter input
 	let currentPage = 1; // Current page for pagination
@@ -18,16 +17,20 @@
 	function generateTimeBlocks() {
 		const blocks = [];
 		const tempTime = new Date(currentTime);
+		tempTime.setMinutes(0, 0, 0);
+
 		while (tempTime <= endTime) {
 			blocks.push(new Date(tempTime));
-			tempTime.setMinutes(tempTime.getMinutes() + 30);
+			tempTime.setHours(tempTime.getHours() + 1);
 		}
+
 		return blocks;
 	}
 
-	function calculateSpan(start, stop) {
-		const duration = (stop - start) / (1000 * 60 * 10); // duration in 10-minute blocks
-		return Math.ceil(duration);
+	function calculateWidth(start, stop) {
+		const duration = (stop - start) / (1000 * 60 * 60);
+		const span = duration * 30;
+		return span;
 	}
 
 	function formatTime(time) {
@@ -35,6 +38,14 @@
 			/(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2}) ([+-]\d{2})(\d{2})/,
 			'$1-$2-$3T$4:$5:$6$7:$8'
 		);
+	}
+
+	function formatHour(time) {
+		const hours = time.getHours();
+		const minutes = time.getMinutes().toString().padStart(2, '0');
+		const ampm = hours >= 12 ? 'PM' : 'AM';
+		const formattedHours = hours % 12 || 12;
+		return `${formattedHours}:${minutes} ${ampm}`;
 	}
 
 	async function fetchEPG() {
@@ -48,33 +59,47 @@
 			const parser = new DOMParser();
 			const xmlDoc = parser.parseFromString(xmlText, 'text/xml');
 
-			const programs = xmlDoc.querySelectorAll('programme');
-			const channelMap = {};
-			Array.from(programs).forEach((program) => {
-				const channel = program.getAttribute('channel').trim();
-				if (!channelMap[channel]) {
-					channelMap[channel] = [];
-				}
-				const title = program.querySelector('title').textContent;
-				const start = new Date(formatTime(program.getAttribute('start')));
-				const stop = new Date(formatTime(program.getAttribute('stop')));
+			const xmlChannels = xmlDoc.querySelectorAll('channel');
+			const xmlPrograms = xmlDoc.querySelectorAll('programme');
 
-				channelMap[channel].push({
-					title,
-					start,
-					stop
+			let channels = [];
+
+			Array.from(xmlChannels).forEach((channel) => {
+				const id = channel.getAttribute('id');
+				const name = channel.querySelector('display-name').textContent;
+				const icon = channel.querySelector('icon')?.getAttribute('src');
+				const programs = [];
+
+				channels.push({
+					id,
+					name,
+					icon,
+					programs
 				});
 			});
 
-			const channels = Object.keys(channelMap);
-			const timeBlocks = generateTimeBlocks();
+			Array.from(xmlPrograms).forEach((program) => {
+				const channelId = program.getAttribute('channel').trim();
+				const channelIndex = channels.findIndex((channel) => channel.id === channelId);
 
-			console.log('Fetched channels:', channels);
-			console.log('Fetched EPG data:', channelMap);
+				if (channelIndex > -1) {
+					const title = program.querySelector('title').textContent;
+					const start = new Date(formatTime(program.getAttribute('start')));
+					const stop = new Date(formatTime(program.getAttribute('stop')));
 
-			setEPGData({ channels, epgData: channelMap, timeBlocks });
+					if (currentTime < stop && start < endTime) {
+						channels[channelIndex].programs.push({
+							title,
+							start,
+							stop
+						});
+					}
+				}
+			});
 
-			console.log('epgStore after setEPGData:', $epgStore);
+			channels = channels.filter((channel) => channel.programs.length > 0);
+
+			setEPGData({ channels, timeBlocks: generateTimeBlocks() });
 		} catch (err) {
 			setEPGError(`Failed to fetch EPG data: ${err.message}`);
 			console.error('Error fetching EPG data:', err);
@@ -88,32 +113,14 @@
 	// Filter the channels based on the filter text
 	$: filteredChannels =
 		channels.length > 0
-			? channels.filter((channel) => channel.toLowerCase().includes(filterText.toLowerCase()))
+			? channels.filter((channel) =>
+					channel.name.toLowerCase().includes(filterText.toLowerCase())
+				)
 			: [];
 
 	// Paginate the filtered channels
 	$: paginatedChannels =
 		filteredChannels.length > 0 ? paginate(filteredChannels, pageSize, currentPage) : [];
-
-	// Filter the EPG data based on the paginated channels
-	$: filteredEPGData = {};
-
-	function filterEPGData() {
-		if (paginatedChannels.length > 0) {
-			for (const channel of paginatedChannels) {
-				const trimmedChannel = channel.trim();
-				if ($epgStore.epgData[trimmedChannel]) {
-					filteredEPGData[trimmedChannel] = $epgStore.epgData[trimmedChannel];
-					console.log(
-						`Data for channel ${trimmedChannel}:`,
-						filteredEPGData[trimmedChannel]
-					);
-				} else {
-					console.log(`No data found for channel ${trimmedChannel}`);
-				}
-			}
-		}
-	}
 
 	function paginate(array, page_size, page_number) {
 		return array.slice((page_number - 1) * page_size, page_number * page_size);
@@ -139,13 +146,7 @@
 	});
 </script>
 
-<input
-	type="text"
-	id="filter-input"
-	on:input={filterEPGData}
-	bind:value={filterText}
-	placeholder="Filter channels"
-/>
+<input type="text" id="filter-input" bind:value={filterText} placeholder="Filter channels" />
 
 {#if $epgStore.isLoading}
 	<div>Loading EPG data...</div>
@@ -154,37 +155,43 @@
 {:else if filterText.length >= 2}
 	<!-- Only render when filter text has at least 2 characters -->
 	<div class="epg-container">
-		{#each paginatedChannels as channel, index}
-			<div class="epg-row" key={index}>
-				<div class="epg-channel-name" title={channel}>{channel}</div>
-				<div class="epg-schedule">
-					{#if filteredEPGData[channel] && filteredEPGData[channel].length > 0}
-						{#each timeBlocks as block, blockIndex}
-							{#if filteredEPGData[channel].some((show) => block >= show.start && block < show.stop)}
-								{#each filteredEPGData[channel].filter((show) => block >= show.start && block < show.stop) as show}
-									<div
-										class="epg-show"
-										style="grid-column: span {calculateSpan(
-											show.start,
-											show.stop
-										)};"
-										title={show.title}
-									>
-										{show.title}
-									</div>
-								{/each}
-							{:else}
-								<div class="epg-show-placeholder">No Show</div>
-							{/if}
-						{/each}
-					{:else}
-						<div class="epg-show-placeholder">No Data</div>
-					{/if}
+		<div class="channels-container">
+			<div class="placeholder channel"></div>
+			{#each paginatedChannels as channel}
+				<div class="channel" title={channel.name}>
+					<img class="channel-icon" src={channel.icon} />
+					<div class="channel-name">{channel.id}</div>
 				</div>
+			{/each}
+			<div class="placeholder channel"></div>
+		</div>
+		<div class="programs-container">
+			<div class="placeholder time-list">
+				{#each timeBlocks as time}
+					<div class="time">{formatHour(time)}</div>
+				{/each}
 			</div>
-		{/each}
-	</div>
+			{#each paginatedChannels as channel}
+				<div class="programs-list">
+					{#each channel.programs as program}
+						<div
+							title="{program.title} - {formatHour(program.start)}"
+							class="program"
+							style="min-width: {calculateWidth(program.start, program.stop)}rem;"
+						>
+							{program.title}
+						</div>
+					{/each}
+				</div>
+			{/each}
 
+			<div class="placeholder time-list">
+				{#each timeBlocks as time}
+					<div class="time">{formatHour(time)}</div>
+				{/each}
+			</div>
+		</div>
+	</div>
 	<!-- Pagination controls -->
 	<div class="pagination-controls">
 		<button on:click={prevPage} disabled={currentPage === 1}>&lt;&lt;</button>
@@ -199,105 +206,85 @@
 	<p>Please enter at least 2 characters to filter the channels.</p>
 {/if}
 
-<!-- Dump data for debugging -->
-{#if filteredChannels.length > 0}
-	<pre>{JSON.stringify(filteredEPGData, null, 2)}</pre>
-{/if}
-
 <style>
-	#filter-input {
-		width: 60rem;
-	}
-
-	/* EPG Container */
 	.epg-container {
 		display: flex;
+		gap: 2rem;
+	}
+
+	.channels-container {
+		display: flex;
 		flex-direction: column;
-		gap: 1rem;
-		overflow-x: auto;
-		height: auto;
-		max-width: 94vw;
-		position: relative;
+		gap: 2rem;
 	}
 
-	/* EPG Row */
-	.epg-row {
-		display: grid;
-		grid-template-columns: 150px 1fr;
+	.channel {
+		height: 5rem;
+		display: flex;
+		align-items: center;
 		gap: 1rem;
 	}
 
-	/* Channel Name */
-	.epg-channel-name {
-		font-weight: bold;
-		padding: 0.5rem;
-		background-color: #333;
-		color: white;
-		border-radius: 5px;
-		text-align: center;
-		position: fixed;
-		width: 20rem;
-		text-overflow: ellipsis;
+	.channel-icon {
+		height: 3rem;
+		width: 3rem;
+		border-radius: 50%;
+		background-color: black;
+	}
+
+	.channel-name {
+		max-width: 15rem;
 		overflow: hidden;
+		text-overflow: ellipsis;
 	}
 
-	/* Schedule Grid */
-	.epg-schedule {
-		display: grid;
-		grid-template-columns: repeat(48, 1fr); /* 48 blocks for 30-min intervals */
+	.programs-container {
+		display: flex;
+		flex-direction: column;
+		gap: 2rem;
+		overflow-y: scroll;
+	}
+
+	.programs-list {
+		display: flex;
 		gap: 0.5rem;
-		grid-auto-flow: column;
-		margin-left: 20rem;
-		grid-auto-columns: min-content;
+		height: 5rem;
+		align-items: center;
 	}
 
-	/* Show Block */
-	.epg-show {
+	.program {
 		background-color: var(--list-background-color);
 		color: white;
 		padding: 0.5rem;
 		border-radius: 5px;
 		text-align: center;
 		white-space: nowrap;
+		height: fit-content;
 		overflow: hidden;
-		text-overflow: ellipsis;
+		text-overflow: ellipsis; /* Truncate text */
 	}
 
-	.epg-show:hover {
+	.program:hover {
 		background-color: var(--list-hover-background-color);
 	}
 
-	.epg-show-placeholder {
-		background-color: #e0e0e0;
-		height: 100%;
-		text-align: center;
-		line-height: 1.5rem;
-	}
-
-	/* Pagination Controls */
-	.pagination-controls {
+	.time-list {
 		display: flex;
-		justify-content: center;
+		gap: 0.5rem;
+		height: 5rem;
 		align-items: center;
-		margin-top: 1rem;
 	}
 
-	.pagination-controls button {
-		background-color: #333;
+	.time {
+		background-color: var(--list-hover-background-color);
 		color: white;
-		padding: 0.5rem 1rem;
-		border: none;
+		padding: 0.5rem;
 		border-radius: 5px;
-		margin: 0 0.5rem;
-		cursor: pointer;
-	}
-
-	.pagination-controls button:disabled {
-		background-color: #666;
-		cursor: not-allowed;
-	}
-
-	.pagination-controls span {
-		font-weight: bold;
+		text-align: center;
+		white-space: nowrap;
+		height: fit-content;
+		min-width: 30rem;
+		overflow: hidden;
+		text-overflow: ellipsis;
 	}
 </style>
