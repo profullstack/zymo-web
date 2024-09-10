e8ed41649a9797fcd30f8d3f21b85a0c0dec1bd6
Anthony Ettinger <anthony@chovy.com>
2024-09-10 05:42:22 -0700

    revert IF NOT EXISTS

--- migrations/nostrusers.sh
+++ migrations/nostrusers.sh
@@ -9,8 +9,8 @@ DATA="DEFINE TABLE nostrusers SCHEMALESS
     FOR create NONE,
 	FOR update WHERE nip05 = \$auth.nip05,
 	FOR delete WHERE nip05 = \$auth.nip05;
-    DEFINE INDEX IF NOT EXISTS idx_name ON nostrusers COLUMNS name UNIQUE;
-	DEFINE INDEX IF NOT EXISTS idx_nip05 ON nostrusers COLUMNS nip05 UNIQUE;
+    DEFINE INDEX idx_name ON nostrusers COLUMNS name UNIQUE;
+	DEFINE INDEX idx_nip05 ON nostrusers COLUMNS nip05 UNIQUE;
 "
 # 	-- FOR select WHERE \$scope = \"allusers\" -- limit to only users in db
 
