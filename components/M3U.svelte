ab3713ec597a71be705651bc76e58a56370908e8
Anthony Ettinger <anthony@chovy.com>
2024-09-13 10:21:26 -0700

    affiliate link

--- components/M3U.svelte
+++ components/M3U.svelte
@@ -44,7 +44,8 @@
 	<p>
 		Need an IPTV service? <a href="https://necroiptv.com/shop/aff.php?aff=990">Get one here!</a>
 		or <a href="https://gomommy.pro">mention code "ZYMOTV" for a 10% discount here</a> or
-		<a href="https://wa.link/iv4q01">here</a>
+		<a href="https://wa.link/iv4q01">here</a> or try
+		<a href="https://argontv.nl/?r=13808">this one</a>.
 	</p>
 	<div class="field">
 		<strong>Filter:</strong>
