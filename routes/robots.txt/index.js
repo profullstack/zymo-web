28255adf37cf75e61a03e416810aff8c62516e3d
Anthony Ettinger <anthony@chovy.com>
2024-09-23 04:09:07 -0700

    fix loadPosts call

--- routes/robots.txt/index.js
+++ routes/robots.txt/index.js
@@ -2,12 +2,6 @@ import view from 'primate/handler/view';
 
 export default {
 	async get() {
-		const { env } = process;
-		const { APP_DOMAIN } = env;
-		const posts = await loadPosts();
-
-		console.log(APP_DOMAIN);
-
 		return view('Robots.hbs', {}, { partial: true });
 	}
 };
