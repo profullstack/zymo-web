37bbc79341c86fce6523c6d90eed12937e3a6305
Anthony Ettinger <anthony@chovy.com>
2024-09-26 06:56:52 -0700

    add lemonsqueezy route.

---
+++ routes/payment/lemonsqueezy/cancel.js
@@ -0,0 +1,36 @@
+import Stripe from 'stripe';
+
+const { env } = process;
+
+export default {
+    async post(request) {
+
+        const { store, session, body } = request;
+        const { subscriptionId }  = body;
+        const { Payment } = store;
+
+        const stripe = new Stripe(env.STRIPE_SK);
+
+        const subscription = await Payment.getBySubscriptionId(subscriptionId)
+    
+        if (subscription.status == "canceled" || subscription.cancelAtPeriodEnd == true)
+            return {
+                error: "subscription already canceled",
+            };
+        try {
+            await stripe.subscriptions.update(subscription.stripeSubscriptionId, {
+                cancel_at_period_end: true,
+            });
+
+            return { success: true };
+            
+        } catch (err) {
+            console.error(err);
+            return  {
+                error: "error cancelling subscription",
+            };
+        }
+
+
+    }
+}
