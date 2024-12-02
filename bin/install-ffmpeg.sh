d79ce0c06ca98a95c8edd76b7812e8b2afe7e930
Anthony Ettinger <anthony@chovy.com>
2024-12-02 09:36:38 -0800

    ffmpeg update

--- bin/install-ffmpeg.sh
+++ bin/install-ffmpeg.sh
@@ -4,8 +4,8 @@ set -e  # Exit on any error
 
 # Define installation directories
 INSTALL_DIR="/usr/local"
-BUILD_DIR="$HOME/ffmpeg_build"
-SOURCE_DIR="$HOME/ffmpeg_sources"
+BUILD_DIR="$HOME/src/ffmpeg_build"
+SOURCE_DIR="$HOME/src/ffmpeg_sources"
 NUM_CORES=$(nproc)
 
 echo "Creating necessary directories..."
@@ -30,7 +30,7 @@ if [ ! -d "$SOURCE_DIR/x264" ]; then
   echo "Building and installing libx264..."
   git clone --depth 1 https://code.videolan.org/videolan/x264.git
   cd x264
-  ./configure --prefix="$BUILD_DIR" --enable-static --enable-pic --disable-opencl
+  ./configure --prefix="$BUILD_DIR" --enable-static --enable-shared --enable-pic --disable-opencl
   make -j$NUM_CORES
   make install
   cd ..
@@ -42,7 +42,7 @@ if [ ! -d "$SOURCE_DIR/x265" ]; then
   git clone --depth 1 https://bitbucket.org/multicoreware/x265_git.git x265
   cd x265/build/linux
   cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$BUILD_DIR" \
-    -DENABLE_SHARED=off -DENABLE_PIC=on ../../source
+    -DENABLE_SHARED=on -DENABLE_PIC=on ../../source
   make -j$NUM_CORES
   make install
   cd ../../../
@@ -54,7 +54,7 @@ if [ ! -d "$SOURCE_DIR/libvpx" ]; then
   git clone --depth 1 https://chromium.googlesource.com/webm/libvpx.git
   cd libvpx
   ./configure --prefix="$BUILD_DIR" --disable-examples --disable-unit-tests \
-    --enable-vp9-highbitdepth --as=yasm
+    --enable-vp9-highbitdepth --as=yasm --enable-shared
   make -j$NUM_CORES
   make install
   cd ..
@@ -66,7 +66,7 @@ if [ ! -d "$SOURCE_DIR/opus" ]; then
   git clone https://github.com/xiph/opus.git
   cd opus
   ./autogen.sh
-  CFLAGS="-fPIC" ./configure --prefix="$BUILD_DIR" --disable-shared
+  CFLAGS="-fPIC" ./configure --prefix="$BUILD_DIR" --disable-static --enable-shared
   make -j$NUM_CORES
   make install
   cd ..
@@ -79,7 +79,7 @@ if [ ! -d "$SOURCE_DIR/aom" ]; then
   mkdir -p aom_build
   cd aom_build
   cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$BUILD_DIR" \
-    -DENABLE_SHARED=off -DENABLE_PIC=on -DENABLE_TESTS=off ../aom
+    -DENABLE_SHARED=on -DENABLE_PIC=on -DENABLE_TESTS=off ../aom
   make -j$NUM_CORES
   make install
   cd ..
@@ -112,17 +112,17 @@ PKG_CONFIG_PATH="$BUILD_DIR/lib/pkgconfig" ./configure \
   --enable-libopus \
   --enable-libass \
   --enable-libvorbis \
-  --enable-libxcb \
   --enable-libaom \
   --enable-libdrm \
   --enable-version3 \
-  --enable-shared
+  --enable-shared \
+  --enable-filter=drawtext
 
 make clean
 make -j$NUM_CORES
 sudo make install
+sudo ldconfig
 
 # Final checks
 echo "FFmpeg installation complete. Verifying installation..."
 ffmpeg -version
-
