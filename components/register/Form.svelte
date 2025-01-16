498440f75ce30c02d3605383c11cdf17e39f9e16
Anthony Ettinger <anthony@chovy.com>
2025-01-16 05:16:13 -0800

    fix replace logic

--- components/register/Form.svelte
+++ components/register/Form.svelte
@@ -25,42 +25,48 @@
 	<script src="https://js.hcaptcha.com/1/api.js" async defer></script>
 </svelte:head>
 
-<h1>Register</h1>
+<section>
+	<h1>Register</h1>
 
-<form method="post" on:submit={onSubmit}>
-	{status ?? ''}
-	<div class="field">
-		<input name="firstName" type="text" placeholder="Enter first name" required />
-	</div>
-	<div>{errors?.firstName ?? ''}</div>
-	<div class="field">
-		<input name="lastName" type="text" placeholder="Enter last name" required />
-	</div>
-	<div>{errors?.lastName ?? ''}</div>
-	<div class="field">
-		<input name="email" type="email" placeholder="Enter email address" required />
-	</div>
-	<div>{errors?.email ?? ''}</div>
-	<div class="field">
-		<input name="username" placeholder="Enter username" required />
-	</div>
-	<div>{errors?.username ?? ''}</div>
-	<div class="field row">
-		<CountrySelect {countries} {status} {errors} />
-		<input name="phone" type="tel" placeholder="Enter phone number" required />
-	</div>
-	<div>{errors?.phone ?? ''}</div>
-	<div class="field">
-		<input name="password" type="password" placeholder="Enter password" required />
-	</div>
-	<div>{errors?.password ?? ''}</div>
-	<div class="field">
-		<input name="password2" type="password" placeholder="Re-enter password" required />
-	</div>
-	<div>{errors?.password2 ?? ''}</div>
-	<div class="field">
-		<div class="h-captcha" data-sitekey=REPLACE_HCAPTCHA_SITE_KEY data-callback="onCaptchaVerify"></div>
-	</div>
-	<div>{errors?.captcha ?? ''}</div>
-	<footer><button type="submit">Register</button></footer>
-</form>
+	<form method="post" on:submit={onSubmit}>
+		{status ?? ''}
+		<div class="field">
+			<input name="firstName" type="text" placeholder="Enter first name" required />
+		</div>
+		<div>{errors?.firstName ?? ''}</div>
+		<div class="field">
+			<input name="lastName" type="text" placeholder="Enter last name" required />
+		</div>
+		<div>{errors?.lastName ?? ''}</div>
+		<div class="field">
+			<input name="email" type="email" placeholder="Enter email address" required />
+		</div>
+		<div>{errors?.email ?? ''}</div>
+		<div class="field">
+			<input name="username" placeholder="Enter username" required />
+		</div>
+		<div>{errors?.username ?? ''}</div>
+		<div class="field row">
+			<CountrySelect {countries} {status} {errors} />
+			<input name="phone" type="tel" placeholder="Enter phone number" required />
+		</div>
+		<div>{errors?.phone ?? ''}</div>
+		<div class="field">
+			<input name="password" type="password" placeholder="Enter password" required />
+		</div>
+		<div>{errors?.password ?? ''}</div>
+		<div class="field">
+			<input name="password2" type="password" placeholder="Re-enter password" required />
+		</div>
+		<div>{errors?.password2 ?? ''}</div>
+		<div class="field">
+			<div
+				class="h-captcha"
+				data-sitekey=REPLACE_HCAPTCHA_SITE_KE
+				data-callback="onCaptchaVerify"
+			></div>
+		</div>
+		<div>{errors?.captcha ?? ''}</div>
+		<footer><button type="submit">Register</button></footer>
+	</form>
+</section>
