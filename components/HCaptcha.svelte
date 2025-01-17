983197d6ec7a9e25ebd84ad3d423da661c06d9d1
Anthony Ettinger <anthony@chovy.com>
2025-01-17 14:09:00 -0800

    hcaptcha

--- components/HCaptcha.svelte
+++ components/HCaptcha.svelte
@@ -1,24 +1,20 @@
 <script>
-	import { onMount } from "svelte";
-	import Status from "@rcompat/http/Status";
-  
+	import { onMount } from 'svelte';
+	import Status from '@rcompat/http/Status';
+
 	export let sitekey;
 	let verified = false;
-  
+
 	onMount(async () => {
-	  await import("@hcaptcha/vanilla-hcaptcha");
-	  document.getElementById("captcha")
-		.addEventListener("verified", async ({ token }) => {
-		  if ((await fetch(`/verify?token=${token}`)).status === Status.OK) {
-			verified = true;
-		  }
+		await import('@hcaptcha/vanilla-hcaptcha');
+		document.getElementById('captcha').addEventListener('verified', async ({ token }) => {
+			if ((await fetch(`/verify?token=${token}`)).status === Status.OK) {
+				verified = true;
+			}
 		});
 	});
-  </script>
-   <h-captcha id="captcha"
-	site-key="{sitekey}"
-	size="normal"
-	theme="dark"></h-captcha>
-  
-  <div>Verified: {verified ? "yes" : "no"}</div>
-  
+</script>
+
+<h-captcha id="captcha" site-key={sitekey} size="normal" theme="dark"></h-captcha>
+
+<div>Verified: {verified ? 'yes' : 'no'}</div>
